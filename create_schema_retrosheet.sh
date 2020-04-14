d5bd21408d4e9a8327f1f15b006b6f2c70ea6c93
Shinichi Nakagawa <spirits.is.my.rader@gmail.com>
2020-04-14 22:37:30 +0900

    retrosheet schema

---
+++ create_schema_retrosheet.sh
@@ -0,0 +1,12 @@
+project=$1
+# Lahman Database
+schema_path='schema/retrosheet/'
+for schema_file in ${schema_path}*.json; do
+  # echo $schema_file
+  schema_name=${schema_file##*/}
+  schema_name=${schema_name%%.*}
+  bq mk --table \
+  --schema $schema_file \
+  --description "" \
+  ${project}:retrosheet.${schema_name}
+done
