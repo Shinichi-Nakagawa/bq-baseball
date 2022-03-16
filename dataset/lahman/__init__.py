d4a7b358c65f932f18417bde6d71daa82924cf68
shinyorke <spirits.is.my.rader@gmail.com>
2022-03-16 16:14:30 +0900

    update

--- dataset/lahman/__init__.py
+++ dataset/lahman/__init__.py
@@ -21,22 +21,10 @@ table_all_star_full = Table(table_name='all_star_full', schema_file=f"{SCHEMA_PA
                             data_source='AllstarFull.csv')
 table_appearances = Table(table_name='appearances', schema_file=f"{SCHEMA_PATH}/appearances.json",
                           data_source='Appearances.csv')
-table_awards_managers = Table(table_name='awards_managers', schema_file=f"{SCHEMA_PATH}/awards_managers.json",
-                              data_source='AwardsManagers.csv')
-table_awards_players = Table(table_name='awards_players', schema_file=f"{SCHEMA_PATH}/awards_players.json",
-                             data_source='AwardsPlayers.csv')
-table_awards_share_managers = Table(table_name='awards_share_managers',
-                                    schema_file=f"{SCHEMA_PATH}/awards_share_managers.json",
-                                    data_source='AwardsShareManagers.csv')
-table_awards_share_players = Table(table_name='awards_share_players',
-                                   schema_file=f"{SCHEMA_PATH}/awards_share_players.json",
-                                   data_source='AwardsSharePlayers.csv')
 table_batting = Table(table_name='batting', schema_file=f"{SCHEMA_PATH}/batting.json", data_source='Batting.csv',
                       load_type=Table.LoadType.DATA_FRAME)
 table_batting_post = Table(table_name='batting_post', schema_file=f"{SCHEMA_PATH}/batting_post.json",
                            data_source='BattingPost.csv', load_type=Table.LoadType.DATA_FRAME)
-table_college_playing = Table(table_name='college_playing', schema_file=f"{SCHEMA_PATH}/college_playing.json",
-                              data_source='CollegePlaying.csv')
 table_fielding = Table(table_name='fielding', schema_file=f"{SCHEMA_PATH}/fielding.json", data_source='Fielding.csv')
 table_fielding_of = Table(table_name='fielding_of', schema_file=f"{SCHEMA_PATH}/fielding_of.json",
                           data_source='FieldingOF.csv')
@@ -44,8 +32,6 @@ table_fielding_of_split = Table(table_name='fielding_of_split', schema_file=f"{S
                                 data_source='FieldingOFsplit.csv')
 table_fielding_post = Table(table_name='fielding_post', schema_file=f"{SCHEMA_PATH}/fielding_post.json",
                             data_source='FieldingPost.csv')
-table_hall_of_fame = Table(table_name='hall_of_fame', schema_file=f"{SCHEMA_PATH}/hall_of_fame.json",
-                           data_source='HallOfFame.csv')
 table_home_games = Table(table_name='home_games', schema_file=f"{SCHEMA_PATH}/home_games.json",
                          data_source='HomeGames.csv', load_type=Table.LoadType.DATA_FRAME)
 table_managers = Table(table_name='managers', schema_file=f"{SCHEMA_PATH}/managers.json", data_source='Managers.csv')
@@ -58,8 +44,6 @@ table_pitching = Table(table_name='pitching', schema_file=f"{SCHEMA_PATH}/pitchi
                        load_type=Table.LoadType.DATA_FRAME)
 table_pitching_post = Table(table_name='pitching_post', schema_file=f"{SCHEMA_PATH}/pitching_post.json",
                             data_source='PitchingPost.csv', load_type=Table.LoadType.DATA_FRAME)
-table_salaries = Table(table_name='salaries', schema_file=f"{SCHEMA_PATH}/salaries.json", data_source='Salaries.csv')
-table_schools = Table(table_name='schools', schema_file=f"{SCHEMA_PATH}/schools.json", data_source='Schools.csv')
 table_series_post = Table(table_name='series_post', schema_file=f"{SCHEMA_PATH}/series_post.json",
                           data_source='SeriesPost.csv')
 table_teams = Table(table_name='teams', schema_file=f"{SCHEMA_PATH}/teams.json", data_source='Teams.csv',
@@ -72,18 +56,12 @@ table_teams_half = Table(table_name='teams_half', schema_file=f"{SCHEMA_PATH}/te
 TABLES = (
     table_all_star_full,
     table_appearances,
-    table_awards_managers,
-    table_awards_players,
-    table_awards_share_managers,
-    table_awards_share_players,
     table_batting,
     table_batting_post,
-    table_college_playing,
     table_fielding,
     table_fielding_of,
     table_fielding_of_split,
     table_fielding_post,
-    table_hall_of_fame,
     table_home_games,
     table_managers,
     table_managers_half,
@@ -91,8 +69,6 @@ TABLES = (
     table_people,
     table_pitching,
     table_pitching_post,
-    table_salaries,
-    table_schools,
     table_series_post,
     table_teams,
     table_teams_franchises,
