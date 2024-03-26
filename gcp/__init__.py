cfd1cb0a8d408fd075ec3af3ef6e9a804543c85e
shinichi.nakagawa@shinyorke <spirits.is.my.rader@gmail.com>
2024-03-26 20:11:44 +0900

    refactor

--- gcp/__init__.py
+++ gcp/__init__.py
@@ -3,15 +3,4 @@ from tempfile import NamedTemporaryFile
 
 
 class Gcp:
-
-    def __init__(self, client):
-        """
-        Create Client
-        :param client: GCP Client class
-        """
-        with NamedTemporaryFile('w') as f:
-            f.write(os.getenv('GCP_AUTH'))
-            f.flush()
-            self.client = client.from_service_account_json(f.name)
-        self.client.project = os.getenv('GCP_PROJECT')
-
+    pass
